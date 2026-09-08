100ba7ecd74f068f19f5901811c4a7b3a223112a
justincodejpeg <justincode.jpeg@gmail.com>
2026-09-07 23:47:06 -0700

    Time: 17 ms (88.28%) | Memory: 108.2 MB (47.11%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hset = new HashSet<Integer>();
+        
+        for (int i = 0; i < nums.length; i ++){
+            
+            if (hset.contains(nums[i])){
+                return true;
+            }
+            
+            hset.add(nums[i]);
+        }
+        
+        return false;
+    }
+}
