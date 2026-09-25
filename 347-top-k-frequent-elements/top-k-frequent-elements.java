92059197cce8e41630ce87b3a6d63d8fff2afd46
justincodejpeg <justincode.jpeg@gmail.com>
2026-09-24 21:59:25 -0700

    Time: 11 ms (96.09%) | Memory: 47.6 MB (66.24%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,52 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
+        HashMap<Integer, Integer> count = new HashMap<>();
 
-        // Step 1: Count frequency of each number
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for (int num : nums){
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
-        // bucket[i] stores numbers having frequency i
-        List<Integer>[] bucket = new ArrayList[n + 1];
+        List<Integer>[] bucket = new List[nums.length + 1];
 
-        // Step 2: Place numbers into their frequency bucket
-        for (int key : map.keySet()) {
-
-            int freq = map.get(key);
-
-            // Create list if bucket is empty
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
+        for ( int key : count.keySet()){
+            int frequency = count.get(key);
+            if (bucket[frequency] == null){
+                bucket[frequency] = new ArrayList<>();
             }
-
-            // Add number into its frequency bucket
-            bucket[freq].add(key);
+            bucket[frequency].add(key);
         }
 
-        int[] ans = new int[k];
-        int index = 0;
+        int[] result = new int[k];
+        int counter = 0;
 
-        // Step 3: Traverse from highest frequency bucket
-        for (int i = bucket.length - 1; i >= 0 && index < k; i--) {
-
-            // Skip empty buckets
-            if (bucket[i] != null) {
-
-                // Take all numbers from current bucket
-                for (int num : bucket[i]) {
-
-                    ans[index++] = num;
-
-                    // Stop once k elements are collected
-                    if (index == k)
-                        break;
+        for (int i = bucket.length - 1; i >= 0 && counter < k; i--) {
+            if (bucket[i] != null){
+                for ( int curr : bucket[i]){
+                    result[counter++] = curr;
                 }
             }
         }
-
-        return ans;
+        return result;
     }
 }
